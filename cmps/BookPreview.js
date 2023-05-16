36a1c1560336b19b7d28effdbab927aa6c32ddd2
esther kaptsan <estherkaptsan@gmail.com>
2023-05-16 21:15:56 +0300

    test

--- cmps/BookPreview.js
+++ cmps/BookPreview.js
@@ -4,7 +4,7 @@ export default {
         <article class="book-preview">
             <img :src="book.thumbnail" />
             <h2>{{ book.title }}</h2>
-            <h2>test, please  work!</h2>
+            <h2>test, please  work!!!!</h2>
             <h3>Price: {{ book.listPrice.amount }} {{ book.listPrice.currencyCode }}</h3>
         </article>
     `,
